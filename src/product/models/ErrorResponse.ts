30bdc1a52ceb5f0c97541cbeac3392de3b7fed12
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-19 13:23:32 +0300

    Custom Error class re-created.

--- src/product/models/ErrorResponse.ts
+++ src/product/models/ErrorResponse.ts
@@ -1,3 +1,16 @@
-export interface IErrorResponse{
-    message?: string
+import { AxiosError } from "axios";
+
+
+class CustomError extends Error {
+    constructor(message: string) {
+        super(message);
+        Object.setPrototypeOf(this, CustomError.prototype);
+    }
+    
+    public static create(message: string): CustomError {
+        return new CustomError(message);
+    }
+    
 }
+
+export default CustomError;
