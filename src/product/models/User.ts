53130b08fca10408a0b5e75ea75d6b2810029a69
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-19 13:24:32 +0300

    User model re-designed.

--- src/product/models/User.ts
+++ src/product/models/User.ts
@@ -1,8 +1,12 @@
+import { IApiModel } from "./Api"
+
 interface IUserModel{
     name:string
     surname:string
     username: string
     email:string
+    apis: Array<IApiModel>
 }
+
 
 export default IUserModel
