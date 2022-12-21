85b6746437b7ee6af2eb7665512b332debebbd11
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-21 17:48:55 +0300

    new screens css codes added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,16 @@ module.exports = {
         width: "width",
         margin: "margin-left"
       }
+
     },
+    screens:{
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'custombp': {'raw': '(max-height: 1234px),(min-width:920px)'}
+    }    
   },
   plugins: [],
 }
