cea22babe1dafd3cc4ec25dd42c3083feb6276d4
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-21 22:31:36 +0300

    Date helper for Charts

--- src/product/utils/format_date.ts
+++ src/product/utils/format_date.ts
@@ -1,11 +1,12 @@
 
-export function formatDate(date: any) {
+export function formatDate(date: Date) {
     return [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1)].join(
       "/"
     );
+
 }
 
-function padTo2Digits(num: any) {
+function padTo2Digits(num: number) {
     return num.toString().padStart(2, "0");
 }
   
