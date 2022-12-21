bae132406b76a824c42440eac7db79e3efb327b9
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-21 17:49:56 +0300

    error response changes.

--- src/product/models/ErrorResponse.ts
+++ src/product/models/ErrorResponse.ts
@@ -1,14 +1,22 @@
 import { AxiosError } from "axios";
 
+type IErrorResponse = {
+    message: string;
+    statusCode: number;
+}
 
 class CustomError extends Error {
-    constructor(message: string) {
+    constructor(message: string, statusCode: number = 500) {
         super(message);
         Object.setPrototypeOf(this, CustomError.prototype);
     }
     
     public static create(message: string): CustomError {
         return new CustomError(message);
+    }
+    
+    public static createFromIErrorResponse(errorResponse: IErrorResponse): CustomError {
+        return new CustomError(errorResponse.message, errorResponse.statusCode);
     }
     
 }
